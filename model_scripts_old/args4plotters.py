a13f074b16a79a089eddc38f467085d236185e6e
KarthikShivaram <kshivara@hawk.iit.edu>
2021-06-08 08:39:27 -0500

    added new tests

---
+++ model_scripts_old/args4plotters.py
@@ -0,0 +1,9 @@
+import config as CONFIG
+
+plotters_args = {"bsl1_sim_vs_score_all_users":{}, 
+                 "plot_online_learning_cumulative":{}, 
+                 "plot_all_cp_online":{}, 
+                 "plot_single_cluster_cumulative":{}, 
+                 "plot_regularization_vs_metrics_at_k_cumu":{},}
+
+plotter_args = {"baseline": [{"bsl1_sim_vs_score_all_users":{}}], "2":[], "3": [], "4": [], "5":[], "6":[], "7":[]}
---
+++ model_scripts_old/attm_config.py
@@ -0,0 +1,18 @@
+import numpy as np
+np.random.seed(24)
+
+def random_search_lr(n,max_lr=0.001,min_lr=0.0000001):
+    """
+    """
+    range_size = (max_lr - min_lr)  # 2
+    lrs = (np.random.rand(n) * range_size + min_lr).tolist()
+    return lrs
+
+# lr = [0.001,0.0001,0.00001]
+lr = [0.001]
+word_weights = [0.1,0.4,0.8]
+print("Learning Rates Used : \n%s"%str(lr))
+epochs = 5
+batch_size = 8
+pickle_file_path = "att_pickle_objs_input"
+save_pickle_results_path = "att_pickle_objs_results"
